e138db216c74ddab0236e008f87866a8eeb5069f
eensander <50321538+eensander@users.noreply.github.com>
2023-08-22 17:18:08 +0200

    chore: small cleanup

--- fair4ai_client/api.py
+++ fair4ai_client/api.py
@@ -32,30 +32,3 @@ class APIService:
         url = f"{self.base_url}/model/{model_id}"
         response = requests.delete(url)
         return response.json()
-
-if __name__ == '__main__':
-    api = APIService()
-
-    print("Print Existing:")
-
-    models = api.list_models()
-    for (model_id, model) in models.items():
-        print(model_id, ':', model['name'])
-
-    print("Print Create:")
-
-    new_model = {"name": "new model!"}
-    create = api.create_model(new_model)
-    create_id = create['id']
-
-    print("new id:", create_id)
-
-    print("Print Read New")
-
-    read_new = api.read_model(create_id)
-
-    print("Contents:", read_new)
-
-    # update = api.update_model(create_id, {"key": "value"})
-
-    # print(update)
